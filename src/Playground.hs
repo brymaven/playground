fcc418a67dd3f0f40591376324b18faf18b7436d
Brymaven <brymaven@gmail.com>
2017-07-21 16:31:42 -0700

    Tic tac toe exercise

--- src/Playground.hs
+++ src/Playground.hs
@@ -4,3 +4,4 @@ module Playground
 
 import qualified Playground.Types as P
 import qualified Playground.Policies as P
+import qualified Playground.Exercises as P
--- src/Playground/Exercises.hs
+++ src/Playground/Exercises.hs
@@ -1,20 +1,42 @@
 module Playground.Exercises where
 
--- module tic tac toe
+import Control.Lens
+import Control.Lens.Tuple
+import Control.Monad (msum)
+
+import Data.Maybe (catMaybes)
+
 -- | (x, y
-type Move = (Int, Int)
+type Move = (Int, Int, Player)
 
 -- | Game is a sequence of moves, with X going first
 type Game = [Move]
 
-data Winner = X | O deriving (Eq, Show)
+data Player
+  = X
+  | O
+  deriving (Eq, Show)
 
-type Board = [[Int]]
--- hasWon :: Board -> Maybe Winner
--- hasWon board =
+type Row = (Player, Player, Player)
+
+-- Need a working tic-tac-toe instance
+type Board = [[Player]]
+
+getWinner :: Board -> Maybe Player
+getWinner =
+  msum .
+  concat .
+  sequence [map wonSeq, map wonSeq . columns, leftDiag, rightDiag]
+  where
+    columns :: Board -> [[Player]]
+    columns = sequence (map (map . (flip (!!))) [0, 1, 2])
+    getIdx idx = pure . wonSeq . catMaybes . sequence (map (flip (^?)) idx)
+    leftDiag = getIdx [ix 2 . ix 2, ix 1 . ix 1, ix 0 . ix 0]
+    rightDiag = getIdx [ix 2 . ix 0, ix 1 . ix 1, ix 0 . ix 2]
+    wonSeq [X, X, X] = Just X
+    wonSeq [O, O, O] = Just O
+    wonSeq _ = Nothing
 
 
--- -- | Runs through the sequences of moves and determines the winner
--- runGame :: Game -> Winner
--- runGame g =
---     foldl
+board :: Board
+board = [[O, X, O], [X, X, X], [O, O, X]]
